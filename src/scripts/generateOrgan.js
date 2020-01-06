66e8956bf34696187159330d6022381b21e23bbc
Glenn McClanan <producersama@gmail.com>
2020-01-05 22:03:38 -0500

    Test circle visualization

--- src/scripts/generateOrgan.js
+++ src/scripts/generateOrgan.js
@@ -173,9 +173,73 @@ export const generateOrgan = (notesList) => {
 
     let canvasWidth, canvasHeight;
 
-    const fftCanvas = document.getElementById("viz-canvas");
-    const fftContext = fftCanvas.getContext("2d");
+    // const fftCanvas = document.getElementById("viz-canvas");
+    // const fftContext = fftCanvas.getContext("2d");
 
+    const canvas = document.getElementById("viz-canvas");
+    const ctx = fftCanvas.getContext("2d");
+
+    // NEW CODE
+    function animationLooper() {
+
+        // set to the size of device
+
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+
+
+        // find the center of the window
+        center_x = canvas.width / 2;
+        center_y = canvas.height / 2;
+        radius = 150;
+
+        // style the background
+        var gradient = ctx.createLinearGradient(0, 0, 0, canvas.height);
+        gradient.addColorStop(0, "rgba(35, 7, 77, 1)");
+        gradient.addColorStop(1, "rgba(204, 83, 51, 1)");
+        ctx.fillStyle = gradient;
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        //draw a circle
+        ctx.beginPath();
+        ctx.arc(center_x, center_y, radius, 0, 2 * Math.PI);
+        ctx.stroke();
+
+        analyser.getByteFrequencyData(frequency_array);
+        for (var i = 0; i < bars; i++) {
+
+            //divide a circle into equal parts
+            rads = Math.PI * 2 / bars;
+
+            bar_height = frequency_array[i] * 0.7;
+
+            // set coordinates
+            x = center_x + Math.cos(rads * i) * (radius);
+            y = center_y + Math.sin(rads * i) * (radius);
+            x_end = center_x + Math.cos(rads * i) * (radius + bar_height);
+            y_end = center_y + Math.sin(rads * i) * (radius + bar_height);
+
+            //draw a bar
+            drawBar(x, y, x_end, y_end, bar_width, frequency_array[i]);
+
+        }
+        window.requestAnimationFrame(animationLooper);
+    }
+
+    // for drawing a bar
+    function drawBar(x1, y1, x2, y2, width, frequency) {
+
+        var lineColor = "rgb(" + frequency + ", " + frequency + ", " + 205 + ")";
+
+        ctx.strokeStyle = lineColor;
+        ctx.lineWidth = width;
+        ctx.beginPath();
+        ctx.moveTo(x1, y1);
+        ctx.lineTo(x2, y2);
+        ctx.stroke();
+    }
+
+    // END NEW CODE
 
     // drawing the FFT
     function drawFFT(values) {
@@ -200,7 +264,6 @@ export const generateOrgan = (notesList) => {
         canvasHeight = fftCanvas.offsetHeight;
         fftContext.canvas.width = canvasWidth;
         fftContext.canvas.height = canvasHeight;
-
     }
 
     function loop() {
