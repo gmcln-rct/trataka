29c720f605399cb420615f515b29e17df8763246
Glenn McClanan <producersama@gmail.com>
2020-01-07 22:04:33 -0500

    Re-add visualizer

--- src/scripts/generateOrgan.js
+++ src/scripts/generateOrgan.js
@@ -164,6 +164,10 @@ export const generateOrgan = (notesList) => {
     // VISUALIZER 
     // Currently just doing FFT
 
+    // ------------------
+    // VISUALIZER
+    // Currently just doing FFT
+
     let fftNum = 4096;
     const fft = new Tone.Analyser("fft", fftNum);
     const waveform = new Tone.Analyser("waveform", 1024);
@@ -173,80 +177,112 @@ export const generateOrgan = (notesList) => {
 
     let canvasWidth, canvasHeight;
 
-    // const fftCanvas = document.getElementById("viz-canvas");
-    // const fftContext = fftCanvas.getContext("2d");
+    const fftCanvas = document.getElementById("viz-canvas");
+    const fftContext = fftCanvas.getContext("2d");
 
-    const canvas = document.getElementById("viz-canvas");
-    const ctx = fftCanvas.getContext("2d");
+
+    // drawing the FFT
+    function drawFFT(values) {
+        fftContext.clearRect(0, 0, canvasWidth, canvasHeight);
+        let x, y, barWidth, val;
+        for (let i = 0, len = values.length; i < len - 1; i++) {
+            barWidth = canvasWidth / len;
+            x = barWidth * i;
+
+            val = Math.abs(values[i] / 255);
+            y = val * canvasHeight;
+            fftContext.fillStyle = "rgba(255, 255, 204, " + val + ")";
+
+            // fftContext.fillStyle = "rgba(31, 178, 204, " + val + ")";
+            fftContext.fillRect(x, canvasHeight - y, barWidth, canvasHeight);
+        }
+    }
+
+    //size the canvases
+    function sizeCanvases() {
+        canvasWidth = fftCanvas.offsetWidth;
+        canvasHeight = fftCanvas.offsetHeight;
+        fftContext.canvas.width = canvasWidth;
+        fftContext.canvas.height = canvasHeight;
+    }
+
+    function loop() {
+        requestAnimationFrame(loop);
+        //get the fft data and draw it
+        drawFFT(fft.getValue());
+        // console.log(fft.getValue());
+
+    }
+
 
     // NEW CODE
 
-    let center_x, center_y, radius, bars,
-        x_end, y_end, bar_height, bar_width,
-        frequency_array;
+    // let center_x, center_y, radius, bars,
+    //     x_end, y_end, bar_height, bar_width,
+    //     frequency_array;
 
-    bars = 200;
-    bar_width = 2;
-
-
-    function animationLooper() {
-
-        // set to the size of device
-
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
+    // bars = 200;
+    // bar_width = 2;
 
 
-        // find the center of the window
-        center_x = canvas.width / 2;
-        center_y = canvas.height / 2;
-        radius = 150;
+    // function animationLooper() {
 
-        // style the background
-        var gradient = ctx.createLinearGradient(0, 0, 0, canvas.height);
-        gradient.addColorStop(0, "rgba(35, 7, 77, 1)");
-        gradient.addColorStop(1, "rgba(204, 83, 51, 1)");
-        ctx.fillStyle = gradient;
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    //     // set to the size of device
 
-        //draw a circle
-        ctx.beginPath();
-        ctx.arc(center_x, center_y, radius, 0, 2 * Math.PI);
-        ctx.stroke();
+    //     canvas.width = window.innerWidth;
+    //     canvas.height = window.innerHeight;
 
-        // analyser.getByteFrequencyData(frequency_array);
-        for (var i = 0; i < bars; i++) {
 
-            //divide a circle into equal parts
-            rads = Math.PI * 2 / bars;
+    //     // find the center of the window
+    //     center_x = canvas.width / 2;
+    //     center_y = canvas.height / 2;
+    //     radius = 150;
 
-            bar_height = fft[i] * 0.7;
+    //     // style the background
+    //     var gradient = ctx.createLinearGradient(0, 0, 0, canvas.height);
+    //     gradient.addColorStop(0, "rgba(35, 7, 77, 1)");
+    //     gradient.addColorStop(1, "rgba(204, 83, 51, 1)");
+    //     ctx.fillStyle = gradient;
+    //     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-            // set coordinates
-            x = center_x + Math.cos(rads * i) * (radius);
-            y = center_y + Math.sin(rads * i) * (radius);
-            x_end = center_x + Math.cos(rads * i) * (radius + bar_height);
-            y_end = center_y + Math.sin(rads * i) * (radius + bar_height);
+    //     //draw a circle
+    //     ctx.beginPath();
+    //     ctx.arc(center_x, center_y, radius, 0, 2 * Math.PI);
+    //     ctx.stroke();
 
-            //draw a bar
-            drawBar(x, y, x_end, y_end, bar_width, fft[i]);
+    //     // analyser.getByteFrequencyData(frequency_array);
+    //     for (var i = 0; i < bars; i++) {
 
-        }
-        // window.requestAnimationFrame(animationLooper);
-    }
+    //         //divide a circle into equal parts
+    //         rads = Math.PI * 2 / bars;
 
-    // for drawing a bar
-    function drawBar(x1, y1, x2, y2, width, frequency) {
+    //         bar_height = fft[i] * 0.7;
 
-        var lineColor = "rgb(" + frequency + ", " + frequency + ", " + 205 + ")";
+    //         // set coordinates
+    //         x = center_x + Math.cos(rads * i) * (radius);
+    //         y = center_y + Math.sin(rads * i) * (radius);
+    //         x_end = center_x + Math.cos(rads * i) * (radius + bar_height);
+    //         y_end = center_y + Math.sin(rads * i) * (radius + bar_height);
 
-        ctx.strokeStyle = lineColor;
-        ctx.lineWidth = width;
-        ctx.beginPath();
-        ctx.moveTo(x1, y1);
-        ctx.lineTo(x2, y2);
-        ctx.stroke();
-    }
+    //         //draw a bar
+    //         drawBar(x, y, x_end, y_end, bar_width, fft[i]);
+
+    //     }
+    //     // window.requestAnimationFrame(animationLooper);
+    // }
+
+    // // for drawing a bar
+    // function drawBar(x1, y1, x2, y2, width, frequency) {
+
+    //     var lineColor = "rgb(" + frequency + ", " + frequency + ", " + 205 + ")";
+
+    //     ctx.strokeStyle = lineColor;
+    //     ctx.lineWidth = width;
+    //     ctx.beginPath();
+    //     ctx.moveTo(x1, y1);
+    //     ctx.lineTo(x2, y2);
+    //     ctx.stroke();
+    // }
 
     // END NEW CODE
 
@@ -268,20 +304,20 @@ export const generateOrgan = (notesList) => {
     // }
 
     // //size the canvases
-    function sizeCanvases() {
-        canvasWidth = fftCanvas.offsetWidth;
-        canvasHeight = fftCanvas.offsetHeight;
-        fftContext.canvas.width = canvasWidth;
-        fftContext.canvas.height = canvasHeight;
-    }
+    // function sizeCanvases() {
+    //     canvasWidth = fftCanvas.offsetWidth;
+    //     canvasHeight = fftCanvas.offsetHeight;
+    //     fftContext.canvas.width = canvasWidth;
+    //     fftContext.canvas.height = canvasHeight;
+    // }
 
-    function loop() {
-        requestAnimationFrame(animationLooper);
-            //get the fft data and draw it
-            drawFFT(fft.getValue());
-            // console.log(fft.getValue());
+    // function loop() {
+    //     requestAnimationFrame(animationLooper);
+    //         //get the fft data and draw it
+    //         drawFFT(fft.getValue());
+    //         // console.log(fft.getValue());
 
-    }
+    // }
 
  
     let synthInterval = setInterval( () => {
