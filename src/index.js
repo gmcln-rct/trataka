c4d325fc5ea3847fda77e4dd80ced8f97c3acb85
Glenn McClanan <producersama@gmail.com>
2020-01-13 15:49:04 -0500

    Experiment with Notes Obj

--- src/index.js
+++ src/index.js
@@ -5,6 +5,26 @@ import { setUpSounds } from './scripts/setUpSounds';
 import { generateOrgan, stopOrgan, _isPlaying } from './scripts/generateOrgan';
 import StartAudioContext from 'startaudiocontext';
 
+let tideObjAlt = {
+    0: ["0.911", "0.005"],
+1: ["0.905", "0.008"],
+2: ["0.901", "0.008"],
+3: ["0.903", "0.007"],
+4: ["0.918", "0.005"],
+5: ["0.921", "0.006"],
+6: ["0.924", "0.010"],
+7: ["0.927", "0.006"],
+8: ["0.934", "0.007"],
+9: ["0.943", "0.005"],
+10: ["0.949", "0.004"],
+11: ["0.951", "0.005"],
+12: ["0.949", "0.005"],
+13: ["0.947", "0.005"],
+14: ["0.949", "0.005"],
+15: ["0.953", "0.004"],
+16: ["0.961", "0.005"]
+};
+
 
 window.addEventListener("DOMContentLoaded", () => {
     let result, notesList, elem;
@@ -30,8 +50,9 @@ window.addEventListener("DOMContentLoaded", () => {
                     ydayCurrents(result)
                         .then(
                             tideObj => {
-                                console.log("Tide Obj: ", tideObj);
-                                notesList = setUpSounds(tideObj);
+                                // console.log("Tide Obj: ", tideObj);
+                                console.log("Tide Obj Alt ", tideObjAlt);
+                                notesList = setUpSounds(tideObjAlt);
                                 generateOrgan(notesList);
                             }
                         )
